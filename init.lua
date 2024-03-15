9e5e6cebabab612ef3e246e42a9f122819962471
Kaan Ozkan <kaan.ozkan@shopify.com>
2024-03-15 11:01:58 -0400

    March update

--- init.lua
+++ init.lua
@@ -47,6 +47,8 @@ vim.g.maplocalleader = ' '
 -- Settings
 -- autocmd! FileType help wincmd L
 vim.opt.cpoptions:append("x")
+vim.opt.scrolloff = 999
+vim.opt.shiftwidth = 2
 
 
 -- [[ Install `lazy.nvim` plugin manager ]]
@@ -413,10 +415,18 @@ end
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
 -- Telescope custom
-vim.keymap.set('n', '<C-j>', ':bprev<CR>', {noremap = true, silent = true})
-vim.keymap.set('n', '<C-k>', ':bnext<CR>', {noremap = true, silent = true})
-vim.keymap.set('n', ';', ':Telescope find_files<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-h>', ':bprev<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-j>', ':bnext<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-l>', ':bd<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', ';', ':Telescope git_files<CR>', {noremap = true, silent = true})
 vim.keymap.set('n', 'rg', ':LiveGrepGitRoot<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-m>', ':Telescope keymaps<CR>', {noremap = true, silent = true})
+
+-- Fugitive custom
+vim.keymap.set('n', '<leader>gs', ':Git<CR>', {desc = 'Git status', noremap = true, silent = true})
+vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>gr', ':Git rebase -i<CR>', {noremap = true, silent = true})
+
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -694,6 +704,55 @@ cmp.setup {
     { name = 'path' },
   },
 }
+
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
+require("lspconfig").ruby_ls.setup({
+  on_attach = function(client, buffer)
+    setup_diagnostics(client, buffer)
+  end,
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
